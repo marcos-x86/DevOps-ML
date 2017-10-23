430f1bb2dbe7b01d953f617732a6a9c6204462ef
marcos-x86 <marcos.x86@outlook.com>
2017-10-23 15:13:20 -0400

    Laboratory01 Vagrant file improved

--- DevOps/labs/lab_02/scripts/oracle-jdk-install.sh
+++ DevOps/labs/lab_02/scripts/oracle-jdk-install.sh
@@ -1,5 +1,6 @@
 wget --continue --no-check-certificate --header "Cookie: oraclelicense=a" 'http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz'
 tar -xf jdk-8u151-linux-x64.tar.gz
+rm jdk-8u151-linux-x64.tar.gz
 mkdir -p /usr/lib/jvm
 mv jdk1.8* /usr/lib/jvm/java-8-oracle
 update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-8-oracle/jre/bin/java 1091
